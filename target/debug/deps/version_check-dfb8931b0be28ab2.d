d281152d3bf589ffcf87afb34eb9e398e4077c04
Kaan Aluclu <kaluclu@vizyonderi.com.tr>
2026-06-11 04:04:10 +0300

    update

---
+++ target/debug/deps/version_check-dfb8931b0be28ab2.d
@@ -0,0 +1,10 @@
+C:\Users\kaluclu\Desktop\steward\target\debug\deps\version_check-dfb8931b0be28ab2.d: C:\Users\kaluclu\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\version_check-0.9.5\src\lib.rs C:\Users\kaluclu\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\version_check-0.9.5\src\version.rs C:\Users\kaluclu\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\version_check-0.9.5\src\channel.rs C:\Users\kaluclu\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\version_check-0.9.5\src\date.rs
+
+C:\Users\kaluclu\Desktop\steward\target\debug\deps\libversion_check-dfb8931b0be28ab2.rlib: C:\Users\kaluclu\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\version_check-0.9.5\src\lib.rs C:\Users\kaluclu\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\version_check-0.9.5\src\version.rs C:\Users\kaluclu\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\version_check-0.9.5\src\channel.rs C:\Users\kaluclu\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\version_check-0.9.5\src\date.rs
+
+C:\Users\kaluclu\Desktop\steward\target\debug\deps\libversion_check-dfb8931b0be28ab2.rmeta: C:\Users\kaluclu\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\version_check-0.9.5\src\lib.rs C:\Users\kaluclu\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\version_check-0.9.5\src\version.rs C:\Users\kaluclu\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\version_check-0.9.5\src\channel.rs C:\Users\kaluclu\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\version_check-0.9.5\src\date.rs
+
+C:\Users\kaluclu\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\version_check-0.9.5\src\lib.rs:
+C:\Users\kaluclu\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\version_check-0.9.5\src\version.rs:
+C:\Users\kaluclu\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\version_check-0.9.5\src\channel.rs:
+C:\Users\kaluclu\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\version_check-0.9.5\src\date.rs:
---
+++ tests/fixtures/dummy.wat
@@ -0,0 +1,5 @@
+(module
+  (func $hello (export "hello") (result i32)
+    i32.const 42
+  )
+)
